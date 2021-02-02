98b7a69cc2d2d2a49f62240bf965f45c33a5267a
William Porto <william.porto@nave.rs>
2021-02-02 16:10:35 -0400

    fix: disabling navigator gestures of @react-navigation/stack to fix memory leak

--- src/routes/AppStack.js
+++ src/routes/AppStack.js
@@ -5,27 +5,24 @@ import {
   TransitionSpecs,
 } from '@react-navigation/stack';
 
-import {ChatScreen, SettingsScreen} from '../screens';
+import {ChatScreen, SettingsScreen} from 'src/screens';
 
-import {darkTheme} from '../styles/themes';
+import {darkTheme} from 'src/styles/themes';
 
 const {Navigator, Screen} = createStackNavigator();
 
 const screenOptions = {
   headerShown: false,
   cardStyle: {backgroundColor: darkTheme.black},
-  gestureEnabled: true,
   ...TransitionPresets.SlideFromRightIOS,
   ...TransitionSpecs.TransitionIOSSpec,
 };
 
-const AppStack = () => {
-  return (
-    <Navigator screenOptions={screenOptions}>
-      <Screen name="ChatScreen" component={ChatScreen} />
-      <Screen name="SettingsScreen" component={SettingsScreen} />
-    </Navigator>
-  );
-};
+const AppStack = () => (
+  <Navigator screenOptions={screenOptions}>
+    <Screen name="ChatScreen" component={ChatScreen} />
+    <Screen name="SettingsScreen" component={SettingsScreen} />
+  </Navigator>
+);
 
 export default AppStack;
