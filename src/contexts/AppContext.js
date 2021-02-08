30582e995752e15a37e0a25ee5a49d4ca807935a
William Porto <william.porto@nave.rs>
2021-02-08 10:27:40 -0400

    refactor: removing ThemeProvider from AppProvider

--- src/contexts/AppContext.js
+++ src/contexts/AppContext.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import {ThemeProvider} from 'styled-components';
 
 import {RealmProvider} from './RealmContext';
 import {UserProvider} from './UserContext';
 import {MessagesProvider} from './MessagesContext';
 
-import {darkTheme as theme} from 'src/styles/themes';
-
 export const AppProvider = ({children}) => (
-  <ThemeProvider theme={theme}>
-    <RealmProvider>
-      <UserProvider>
-        <MessagesProvider>{children}</MessagesProvider>
-      </UserProvider>
-    </RealmProvider>
-  </ThemeProvider>
+  <RealmProvider>
+    <UserProvider>
+      <MessagesProvider>{children}</MessagesProvider>
+    </UserProvider>
+  </RealmProvider>
 );
